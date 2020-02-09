d198cad776b2841881d4dcc455f1ba43bb7bff4c
SadCat88 <SadCat88@yandex.ru>
2020-02-09 15:22:22 +0300

    Секция work. Галерея картинок baguetteBox.js.

--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,16 @@
 'use strict';
 
 console.log('index.js is run');
+
+baguetteBox.run('.gallery', {
+  animation: 'fadeIn',
+  // затухающая анимация
+  noScrollbars: true,
+  // убирает полосу прокрутки основной страницы
+
+  async: true,
+  // === добавляет подпись внизу из атрибута alt
+  captions: function(element) {
+    return element.getElementsByTagName('img')[0].alt;
+  },
+});
